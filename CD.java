76df367f3ad2bbafeb56b727a2086cc2d015e5dc
Jack Carroll <jhc8@email.sc.edu>
2021-04-19 12:05:11 -0400

    Added method definitions

--- CD.java
+++ CD.java
@@ -7,32 +7,44 @@ public class CD implements DigitalAlbum {
     private ArrayList<String> songs;
     private int currentIndex;
 
-    public CD(String song1, String song2) {
-        // TODO
+    public CD(String song1, String song2, String song3, String song4, String song5) {
+        songs.add(song1);
+        songs.add(song2);
+        songs.add(song3);
+        songs.add(song4);
+        songs.add(song5);
     }
 
     public String playFromBeginning() {
-        // TODO
+        return "Playing song 1: " + songs.get(0);
+        // Will also advance to the next song
     }
 
     public String playSong(int num) {
-        // TODO
+        if(num < songs.size() && num > 0)
+            return "Playing: " + songs.get(currentIndex);
+        return "Not a valid song number";
     }
 
     public String prevSong() {
-        // TODO
+        if(currentIndex - 1 > 0)
+            return "Skipping back and playing: " + songs.get(currentIndex);
+        return "Already at the beginning";
     }
 
     public String nextSong() {
-        // TODO
+        if(currentIndex + 1 < songs.size())
+            return "Playing " + currentIndex + ":" + songs.get(currentIndex);
+        return "Playing 1: " + songs.get(0);
     }
 
     public String stop() {
-        // TODO
+        currentIndex = 0;
+        return "Stopping digital album";
     }
 
     public String pause() {
-        // TODO
+        return "Pausing " + songs.get(currentIndex);
     }
     
 }
